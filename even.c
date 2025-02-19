fabf14cd2c1716fd9c2740348779f9a942152e47
ASDFGH-ALEX <alexsabu053@gmail.com>
2025-02-19 21:50:47 +0530

    Update even.c

--- even.c
+++ even.c
@@ -1,1 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int range_1,range_2,i,sum=0;
+    printf("Enter the first range:");
+    scanf("%d",&range_1);
+    printf("Enter the second range:");
+    scanf("%d",&range_2);
+    for(i=range_1;i<range_2;i++)
+    {
+        if(i%2==0)
+        {
+            sum+=i;
+        }
+    }
+printf("The sum of even numbers from %d to %d is %d",range_1,range_2,sum);
+return 0;
+}
 
