f95d8afca58ab5a094f1407473f52bc6db942113
ASDFGH-ALEX <alexsabu053@gmail.com>
2025-02-19 20:36:58 +0530

    Create Vowel.c

---
+++ Vowel.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    char C1;
+    printf("Enter the character:");
+    scanf(" %c",&C1);
+    if(C1=='a')
+    {
+        printf("The given character %c is a vowel",C1);
+    }
+    else if(C1=='e')
+    {
+        printf("The given character %c is a vowel",C1);
+    }
+    else if(C1=='i')
+    {
+        printf("The given character %c is a vowel",C1);
+    }
+    else if(C1=='o')
+    {
+        printf("The given character %c is a vowel",C1);
+    }
+    else if(C1=='u')
+    {
+        printf("The given character %c is a vowel",C1);
+    }
+    else
+    {
+        printf("The given character is a consonant");
+    }
+return 0;
+}
