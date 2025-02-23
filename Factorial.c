5377b9c1da7db853e290d33b5ed8affc7acd398f
ASDFGH-ALEX <alexsabu053@gmail.com>
2025-02-23 07:56:58 +0530

    Update Factorial.c

--- Factorial.c
+++ Factorial.c
@@ -1,16 +1,21 @@
 #include<stdio.h>
 int main()
 {
-    int i,f,num;
+    int i,factorial,num;
     printf("Enter the number:");
     scanf("%d",&num);
     i=1;
-    f=1;
-    while(i<num)
+    factorial=1;
+    if(num<0)
     {
-        f*=i;
+        printf("Invalid entry.\nPlease enter a positive integer");
+        return 0;
+    }
+    while(i<=num)
+    {
+        factorial*=i;
         i++;
     }
-printf("factorial of %d is %d",num,f);
+    printf("The factorial of %d is:%d",num,factorial);
 return 0;
 }
