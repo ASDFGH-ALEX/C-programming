3d3c4f590fbd772316f77c18851986080cd288bc
ASDFGH-ALEX <alexsabu053@gmail.com>
2025-03-21 18:23:00 +0530

    Update Search_an_element_in_array.c

--- Search_an_element_in_array.c
+++ Search_an_element_in_array.c
@@ -1,1 +1,34 @@
-
+//Proram to search for an element 
+#include<stdio.h>
+int main()
+{
+    int a[20],n,i,key,pos=-1;
+    printf("Enter the size of the array:");
+    scanf("%d",&n);
+    printf("Enter the elements of the array:");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("Enter the element to be searched:");
+    scanf("%d",&key);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==key)
+        {
+            key=a[i];
+            pos=i;
+            break;
+        }
+    }
+        if(pos==-1)
+        {
+            
+            printf("The element is not found");
+        }
+        else
+        {
+            printf("The element %d is found \nAt the position of:%d",key,pos);
+        }
+return 0;
+}
